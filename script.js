9f6a0a6fc13bb07cec1418eb1e843fdb0b0bfc8b
Mubashir Ahmed Abdullah <155999512+MubashirAhmedAbdullah@users.noreply.github.com>
2024-06-26 09:38:31 +0500

    assigment portal one files uploaded and updated

---
+++ script.js
@@ -0,0 +1,303 @@
+
+var assigments =document.getElementById('assigment_list'); 
+
+
+var assigment_list = [
+    // {
+    //     name: "Assigment 27" ,
+    //     discription: "Make a to do lister app and you have to make the login page and login in to do app web veiw and save the login user id and to do's of that user id in local storage",
+    //     date: "08-June-2024",
+    //     hosted_link: "https://localstoragetodolister.netlify.app/",
+    //     Github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%2017",
+    // },
+
+    {
+        assignment_no: "Assigment 27",
+        assignment_name: "To Do Lister By Using LocalStorage",
+        description: "Make a to do lister and and save the data of several user in local storage",
+        assignment_date: "08-June-2024",
+        hosted_link: "https://localstoragetodolister.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%2017",
+        image: "./assest/images/27.png"
+    },
+    
+    {
+        assignment_no: "Assigment 26",
+        assignment_name: "Cards Using For Each",
+        description: "Make cards useing foreach and add in it update button and delete button",
+        assignment_date: "30-May-2024",
+        hosted_link: "https://cardsforeach.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%2016",
+        image: "./assest/images/26.png"
+    },
+    {
+        assignment_no: "Assigment 25",
+        assignment_name: "Attendence Portal",
+        description: "Make A assigment portal in which you take attendence of students and show on the top of the table how many students are present ,absent,and on leave",
+        assignment_date: "25-May-2024",
+        hosted_link: "https://attendenceportal.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%2015",
+        image: "./assest/images/25.png"
+    },
+    {
+        assignment_no: "Assigment 24",
+        assignment_name: "To Do Lister",
+        description: "Make A to do lister in which you add what you do and also edit or delete it",
+        assignment_date: "23-May-2024",
+        hosted_link: "https://todolisterrr.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%2014",
+        image: "./assest/images/24.png"
+    },
+    {
+        assignment_no: "Assigment 23",
+        assignment_name: "Text Editor",
+        description: "Make a text editor in which you can make your text bold,italic,change text aligment,change text color etc,",
+        assignment_date: "19-May-2024",
+        hosted_link: "https://texteditorwebapp.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%2013",
+        image: "./assest/images/23.png"
+    },
+    {
+        assignment_no: "Assigment 22",
+        assignment_name: "JavaScript Bulb",
+        description: "Make a bulb by using javascript in which we on button bulb and button off bulb off ",
+        assignment_date: "16-May-2024",
+        hosted_link: "https://bulbbyma.netlify.app/?authuser=0",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%2012?authuser=0",
+        image: "./assest/images/22.png"
+    },
+    {
+        assignment_no: "Assigment 21",
+        assignment_name: "JavaScript Counter Assigment",
+        description: "Make A counter with javaScript contain three buttons first for add,Second for Subtract,Third for reset",
+        assignment_date: "14-May-2024",
+        hosted_link: "https://counterappbyma.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%2011",
+        image: "./assest/images/21.png"
+    },
+    {
+        assignment_no: "Assigment 20",
+        assignment_name: "Finance Assigment",
+        description: "Make Finace App or Web which calculate your income ,expense and tell you about your saveing.",
+        assignment_date: "12-May-2024",
+        hosted_link: "https://financetrackerappp.netlify.app/?authuser=0",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%2010?authuser=0",
+        image: "./assest/images/20.png"
+    },
+    {
+        assignment_no: "Assigment 19",
+        assignment_name: "Calculator",
+        description: "Make a calculator.",
+        assignment_date: "09-May-2024",
+        hosted_link: "https://java-scriptcalculator.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/Java%20Script%20Assigment%209mmj",
+        image: "./assest/images/19.png"
+    },
+    {
+        assignment_no: "Assigment 18",
+        assignment_name: "Eid-Ul-Adha Countdown",
+        description: "Make a countdown for (Eid-Ul-Adha) which counts how many months,days,hours,minutes left in (Eid-Ul-Adha).",
+        assignment_date: "05-May-2024",
+        hosted_link: "https://eidcountdownbymubashirahmed.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%206",
+        image: "./assest/images/18.png"
+    },
+    {
+        assignment_no: "Assigment 17",
+        assignment_name: "Digital Clock",
+        description: "Make a digital clock and also show the current day,month,year.",
+        assignment_date: "01-May-2024",
+        hosted_link: "https://profound-crostata-04289b.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%205",
+        image: "./assest/images/17.png"
+    },
+    {
+        assignment_no: "Assigment 16",
+        assignment_name: "Guess The number game",
+        description: "Make a guess the number (1-6) game by using JavaScript Math.random() method.",
+        assignment_date: "30-Apr-2024",
+        hosted_link: "https://assigment123.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%204",
+        // image: "./assest/images/26.png"
+    },
+    {
+        assignment_no: "Assigment 15",
+        assignment_name: "JavaScript variables and mathamatical operations.",
+        description: "No dicription",
+        assignment_date: "11-Mar-2024",
+        hosted_link: "https://assigment123.netlify.app/Not%20Found",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/JavaScript%20Assigment%201",
+        // image: "./assest/images/26.png"
+    },
+    {
+        assignment_no: "Assigment 14",
+        assignment_name: "Apple Landing Page Clone",
+        description: "Make a clone of Apple.com landing page .",
+        assignment_date: "14-Feb-2024",
+        hosted_link: "https://appleclonetest.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/ASSIGMENT%2014",
+        image: "./assest/images/apple.png"
+    },
+
+    {
+        assignment_no: "Assigment 13",
+        assignment_name: "Responsive web page ",
+        description: "Make A Responsive Web Template Useing Bootstrap Grid System.",
+        assignment_date: "12-Feb-2024",
+        hosted_link: "https://hilarious-peony-e916d9.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/ASSIGMENT%2013",
+        image: "./assest/images/colorif.png"
+    },
+
+    {
+        assignment_no: "Assigment 12",
+        assignment_name: "Responsive cards",
+        description: "This template is not so hard to design. Make this with the same responsive behavior on different screen sizes.",
+        assignment_date: "07-Feb-2024",
+        hosted_link: "https://spontaneous-croissant-d6abb4.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/ASSIGMENT%2012",
+        image: "./assest/images/template\ 2.png"
+    },
+
+
+    {
+        assignment_no: "Assigment 11",
+        assignment_name: "Amazon (Landing page) clone",
+        description: "Make Amazon Clone of the landing page.",
+        assignment_date: "05-Feb-2024",
+        hosted_link: "https://amazoncolone.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/Assigment%2011",
+        image: "./assest/images/amazon.png"
+    },
+
+
+    {
+        assignment_no: "Assigment 10",
+        assignment_name: "Travels and Tour's web template.",
+        description: "Design a template to enhance your CSS skills in making responsive web pages. This template is quite simple..",
+        assignment_date: "02-Feb-2024",
+        hosted_link: "https://mubashirtravels.netlify.app/?authuser=0",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/ASSIGMENT%209?authuser=0",
+        image: "./assest/images/travels\ and\ tours.png"
+    },
+
+
+    {
+        assignment_no:"Assigment 9",
+        assignment_name: "Design a responsive template",
+        description: "Make a responsive template by using flex .",
+        assignment_date: "29-Jan-2024",
+        hosted_link: "https://assigment123.netlify.app/NOT%20FOUND",
+        github_link: "https://assigment123.netlify.app/NOT%20FOUND",
+        image: "./assest/images/template\ 1.png"
+    },
+
+
+    {
+        assignment_no:"Assigment 8",
+        assignment_name: "Color Plate",
+        description: "Make a design of responsive color plates by using flex.",
+        assignment_date: "26-Jan-2024",
+        hosted_link: "https://assigment123.netlify.app/NOT%20FOUND",
+        github_link: "https://assigment123.netlify.app/NOT%20FOUND",
+        image: "./assest/images/color\ plate.png"
+    },
+
+
+    {
+        assignment_no:"Assigment 7",
+        assignment_name: "Facebook Login Page ",
+        description: "Make a clone web page of FaceBook login page.",
+        assignment_date: "19-Jan-2024",
+        hosted_link: "https://curious-peony-534991.netlify.app/?authuser=0",
+        github_link: "https://github.com/MubashirAhmedAbdullah/smit-/blob/master/index.html?authuser=0",
+        image: "./assest/images/facebook.png"
+    },
+
+
+    {
+        assignment_no:"Assigment 6",
+        assignment_name: "Upwork (Landing Page)",
+        description: "You have to make the clone of Upwork Website.",
+        assignment_date: "19-Jan-2024",
+        hosted_link: "https://taupe-tapioca-01fd83.netlify.app/?authuser=0",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/ASSIGMENT%205?authuser=0",
+        image: "./assest/images/upwork.png"
+    },
+
+
+    {
+        assignment_no:"Assigment 5",
+        assignment_name: "ABC book",
+        description: "Make Kids book (ABC book).",
+        assignment_date: "15-Jan-2024",
+        hosted_link: "https://glossaryassigment5.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/ASSIGMENT%205",
+        image: "./assest/images/abd\ book.png"
+    },
+
+
+    {
+        assignment_no:"Assigment 4",
+        assignment_name: "Table with CSS",
+        description: "Make the table and also style it with CSS.",
+        assignment_date: "03-Jan-2024",
+        hosted_link: "https://assigment123.netlify.app/Not%20Found/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/ASSIGMENT%204",
+        image: "./assest/images/css\ table.png"
+    },
+
+
+    {
+        assignment_no:"Assigment 3",
+        assignment_name: "Complete Web",
+        description: "Make complete website containing five pages.",
+        assignment_date: "01-Jan-2024",
+        hosted_link: "https://sirjeecoachingcenter.netlify.app/",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/ASSIGMENT%203",
+        image: "./assest/images/sirjee.png"
+    },
+
+
+    {
+        assignment_no:"Assigment 2",
+        assignment_name: "Periodic Table",
+        description: "Make a periodic table with the help of HTML.",
+        assignment_date: "27-Dec-2024",
+        hosted_link: "https://assigment123.netlify.app/Not%20Found",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/ASSIGMENT%202",
+        image: "./assest/images/periodic\ table.png"
+    },
+
+
+    {
+        assignment_no:"Assigment 1",
+        assignment_name: "Resume",
+        description: " make your simple CV/Resume with HTML tags.",
+        assignment_date: "20-Dec-2024",
+        hosted_link: "https://assigment123.netlify.app/Not%20Found",
+        github_link: "https://github.com/MubashirAhmedAbdullah/Assigments/tree/master/ASSIGMENT%201",
+        image: "./assest/images/1.webp"
+    },
+
+    
+]
+
+assigment_list.forEach(function(data, ind){
+    var element = ` <div class="assignment">
+    <h2>${data.assignment_no}</h2>
+    <p>Description: ${data.description}</p>
+    <p>Deadline: ${data.assignment_date}</p>
+    <div class="link-container">
+        <div>
+            <a href="${hosted_link}" class="hosting-link">Hosting Link</a>
+        </div>
+        <div>
+            <a href="${github_link}" class="github-link">GitHub Link</a>
+        </div>
+    </div>
+    </div>`
+
+
+    assigment_list.innerHTML += element;
+})
